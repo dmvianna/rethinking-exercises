0604b858089e8117b7e287a3de3fb11cccf81756
Daniel Vianna <dmlvianna@gmail.com>
2020-10-28 15:20:36 +1100

    add responses to exercises

--- ch7.outliers.r
+++ ch7.outliers.r
@@ -76,3 +76,37 @@ WAIC_m5.3t <- WAIC( m5.3t , pointwise=TRUE )
 plot( PSIS_m5.3t$k , WAIC_m5.3t$penalty , xlab="PSIS Pareto k" ,
      ylab="WAIC penalty" , col=rangi2 , lwd=2 )
 identify( x=PSIS_m5.3t$k , y=WAIC_m5.3t$penalty , labels=d$Loc )
+
+## 7E1
+## 1. The measure of uncertainty should increase linearly as a function of the probability of an event
+## 2. The measure of uncertainty should increase as the number of potential events increases
+## 3. The measure of uncertainty should be additive. That is, it should be possible to add its component measures of uncertainty.
+
+## 7E2
+p <- c( 0.3 , 0.7 )
+-sum( p * log(p) )
+## 0.61
+
+## 7E3
+p <- c( 0.2 , 0.25 , 0.25 , 0.3 )
+-sum( p * log(p) )
+## 1.38
+
+## 7E4
+p <- c( 1/3 , 1/3 , 1/3 )
+-sum( p * log(p) )
+# 1.10
+
+## 7M2
+## Model Selection: Chooses the model that's best at predicting
+## Model Comparison: Doesn't discard models, so we can study how variables influence prediction. This can be valuable information for causal inference.
+
+## 7M3
+## Individual observations might be highly influential. Given that a single extreme observation might is unlikely to be reproduced in all future samples, these are indicative of how likely the model is to overfit.
+
+## 7M5
+## Informative priors reduce the model's sensitivity to data. It assigns less likelihood to extreme values.
+
+## 7M6
+## Overly informative priors reduce the model's sensitivity to data to an extent akin to ignoring relevant information contained therein.
+
